4a74dc18e62242b5386852aa0692f0c33ebbd537
Paul A. Beata <pbeata@users.noreply.github.com>
2017-07-04 09:30:16 -0400

    changed first column of SIGNAL(s) in MAIN: removed time from array

--- InverseModel/converted_IFM/source/MAIN_multiroom_multifire_1d_xc.py
+++ InverseModel/converted_IFM/source/MAIN_multiroom_multifire_1d_xc.py
@@ -9,31 +9,24 @@ import read_signal_xc
 
 #GLOBAL VALUES FOR CONFIG
 numcomp = 4
-numfire = 4  #numfire have to be smaller than numcomp, fire1 in comp1, fire2 in comp2, etc
+numfire = 4    #numfire have to be smaller than numcomp, fire1 in comp1, fire2 in comp2, etc
 numsignal = 8
 
 #SCRIPT TO RUN WHOLE CODE
 tic = time.time()
 #----------------------------------------------------------------------------------------%
 
-#INPUT VARIABLES
+#INPUT DATA
 #the format of data should be a matrix with [time1, hrr1, time2, hrr2...]
-inputPath = '../input/data4.mat'
-
-#a = load(inputPath)
-data = np.loadtxt("../input/data4.txt")
-
-mdata = data.shape[0]
-ndata = data.shape[1]
-
-#hrrin = a.hrr
-hrrin = data[:, 1:ndata]
+inData = np.loadtxt("../input/data4.txt")
+mdata = inData.shape[0]
+ndata = inData.shape[1]
+hrrin = inData[:, 1:ndata]
+timein = inData[:,0]
 #print("size of hrrin is " + str(hrrin.shape[0]) + " by " + str(hrrin.shape[1]))
-
-#timein = a.t
-timein = data[:,0]
 #print("length of timein is " + str(len(timein)))
 
+#PLOT INITIAL HRR CURVES
 plt.close('all')
 f, axarr = plt.subplots(numfire, sharex=True)
 for counter in range(0, numfire):
@@ -46,65 +39,64 @@ for counter in range(0, numfire):
 print("To show the initial HRR plots, uncomment #plt.show()")    
 #plt.show()
 
+#CREATE AND READ REAL CONCENTRATIONS AND FLOWS
 create_signal_xc.create_signal_xc_func(timein, hrrin, numcomp, numfire, 'exp_signal_xc')
 SIGNAL_exp = read_signal_xc.read_signal_xc_func(numcomp, 'exp_signal_xc')
-TIME_exp = SIGNAL_exp[:, 0]
 
+#GET TIME FROM COLUMN 0; THEN DELETE IT FROM SIGNAL
+TIME_exp = SIGNAL_exp[:, 0]
+numTime = len(TIME_exp)
+#print("size of SIGNAL_exp is " + str(SIGNAL_exp.shape[0]) + " by " + str(SIGNAL_exp.shape[1]))
+SIGNAL_exp = np.delete(SIGNAL_exp, [0], axis=1)
+#print("(post-delete) size of SIGNAL_exp is " + str(SIGNAL_exp.shape[0]) + " by " + str(SIGNAL_exp.shape[1]))
+
+#INIALIZE VARIABLES
 num_fail_allowed = 5
 iteration_max = 20
-#iteration_turn=20;
 error_tol = 0.001
 error_max = 0.1
-
-n = len(TIME_exp)
-HRR_pred = np.zeros((n, numfire)) + 1000.0
-least_error = np.zeros((n, 1)) + 100.0
-SIGNAL_pred = np.zeros((n, numsignal)) + 20.0  #For now signal is temperature
-SIGNAL_turb = SIGNAL_pred
 num_fail = 0
-#error_extra = 0
 HRR_delta_base = 0.01
 HRR_low = 5000.0
-#HRR_delta_lowhrr = 50000
-
+# Paul: we should rename k with something more descriptive
 k = 1.0
-factor = 1.0
+k_avg = 0.0
 HRR_max = 6.0 * 10.0 ** 5
 HRR_min = 0.0
 total_iteration = 0
 low_tol = 0.001
 extra_tol = 0.0
 jump_tol = 1.0
-k_avg = 0.0
-HRR_temp = np.zeros((1, numfire)) + 1000.0
-SIGNAL_lowfire = [0, 0, 0, 0]
-least_error = np.zeros(n)
 
-# CHANGE 4 TO n FOR FINAL
+#INITIALIZE ARRAYS
+HRR_pred = 1000.0 * np.ones((numTime, numfire))
+least_error = 100.0 * np.ones((numTime, 1))
+#(for now signal is temperature)
+SIGNAL_pred = 20.0 * np.ones((numTime, numsignal))
+SIGNAL_turb = SIGNAL_pred
+HRR_temp = 1000.0 * np.ones((1, numfire))
+SIGNAL_lowfire = [0, 0, 0, 0]
+
+# CHANGE 4 TO numTime FOR FINAL
 for i in range(1, 4):
   print("Current Time = " + str(TIME_exp[i]))
   HRR_pred[i,:] = HRR_temp
 
   create_signal_xc.create_signal_xc_func(TIME_exp, HRR_pred, numcomp, numfire, 'pred_signal_xc')
   SIGNAL_pred = read_signal_xc.read_signal_xc_func(numcomp, 'pred_signal_xc')
-    
-  #SIGNAL_diff = np.zeros((1, numsignal+1))
-  #for j in range(0, numfire):
+  SIGNAL_pred = np.delete(SIGNAL_pred, [0], axis=1)
+  #print("size of SIGNAL_pred is " + str(SIGNAL_pred.shape[0]) + " by " + str(SIGNAL_pred.shape[1]))
 
-  # Paul: FOUND USE OF MAGIC NUMBERS == CHECK THIS SEGMENT
-  SIGNAL_diff = SIGNAL_pred[i,1:5] - SIGNAL_exp[i,1:5]
-  #SIGNAL_diff[0,1:5] = SIGNAL_pred[i,1:5] - SIGNAL_exp[i,1:5]
+  # Paul: FOUND USE OF MAGIC NUMBERS == CHECK THIS SEGMENT (0:4)
+  SIGNAL_diff = SIGNAL_pred[i,0:4] - SIGNAL_exp[i,0:4]
   max_SIGNAL_diff = max(abs(SIGNAL_diff))
   max_fire = np.argmax(abs(SIGNAL_diff))
-  # Paul: (magic numbers above are 1:5)
-
-  #print(SIGNAL_diff)
   print("Max signal diff and max fire: " + str(max_SIGNAL_diff) + "  " + str(max_fire))
 
   least_error[i] = max_SIGNAL_diff
   iteration = 1
   HRR_new = 1000.0
-  #lag = 0
+  # Paul: we should rename factor with something more descriptive
   factor = 1.0
 
   while (max_SIGNAL_diff > error_tol):
@@ -119,34 +111,42 @@ for i in range(1, 4):
 
     create_signal_xc.create_signal_xc_func(TIME_exp, HRR_turb, numcomp, numfire, 'pred_signal_xc')
     SIGNAL_turb = read_signal_xc.read_signal_xc_func(numcomp, 'pred_signal_xc')
+    SIGNAL_turb = np.delete(SIGNAL_turb, [0], axis=1)
 
     # Paul: FOUND MAGIC NUMBER (4)
+    # Paul: check this (4) because it might be wrong in the 0-based Python indexing
     k1 = SIGNAL_turb[i, max_fire + int(SIGNAL_lowfire[max_fire] * 4)]
     k2 = SIGNAL_pred[i, max_fire + int(SIGNAL_lowfire[max_fire] * 4)]
+
+    # Paul: WE NEED TO RENAME k TO SOMETHING MORE DESCRIPTIVE
     k = (k1 - k2) / HRR_delta
+
+    # Get new HRR
+    HRR_new = HRR_pred[i, max_fire] - SIGNAL_diff[max_fire] / k * factor
+    if (HRR_new > HRR_max):
+      HRR_new = HRR_max
+      factor = 0.5 * factor                   # magic number
+    elif (HRR_new < HRR_min):
+      HRR_new = 0.1 * max(HRR_pred[i-1, :])   # magic number
+      factor = 0.5 * factor                   # magic number
+    else:
+      factor = min(1.0, factor * 1.5)         # magic numbers
+
+    HRR_pred[i, max_fire] = HRR_new
+    create_signal_xc.create_signal_xc_func(TIME_exp, HRR_pred, numcomp, numfire, 'pred_signal_xc')
+    SIGNAL_pred = read_signal_xc.read_signal_xc_func(numcomp, 'pred_signal_xc')
+    SIGNAL_pred = np.delete(SIGNAL_pred, [0], axis=1)
+
+    # Paul: FOUND USE OF MAGIC NUMBERS == CHECK THIS SEGMENT (0:4)
+    SIGNAL_diff = SIGNAL_pred[i,0:4] - SIGNAL_exp[i,0:4]
+    max_SIGNAL_diff = max(abs(SIGNAL_diff))
+    max_fire = np.argmax(abs(SIGNAL_diff))
+    print("Max signal diff and max fire: " + str(max_SIGNAL_diff) + "  " + str(max_fire))
 
     # terminate while loop early for testing
     max_SIGNAL_diff = 0.0
 
     '''
-        HRR_new = HRR_pred(i, max_fire) - SIGNAL_diff(max_fire) / k * factor    #get new hrr
-
-        if (HRR_new > HRR_max):
-            HRR_new = HRR_max
-            factor = 0.5 * factor
-        elif (HRR_new < HRR_min):
-            HRR_new = 0.1 * max(HRR_pred(i - 1, mslice[:]))
-            factor = 0.5 * factor
-        else:
-            factor = min(1, factor * 1.5)
-        end
-
-        HRR_pred(i, max_fire).lvalue = HRR_new
-        create_pred_signal_xc(time, HRR_pred, numcomp, numfire)
-        read_pred_signal_xc(numcomp)
-        SIGNAL_diff = SIGNAL_pred(i, mslice[1:4]) - SIGNAL_exp(i, mslice[1:4])
-        [max_SIGNAL_diff, max_fire] = max(abs(SIGNAL_diff)); print max_SIGNAL_diff; print max_fire    #update signal &signal_diff
-
         #         if (HRR_pred(i-1,max_fire)<max(HRR_pred(i-1,:))/10.0 &&
         #         HRR_pred(i,max_fire)<0.6*max(HRR_pred(i-1,:)))
         if HRR_pred(i, max_fire) < 0.2 * (HRR_pred(i - 1, max_fire)) or (HRR_pred(i - 1, max_fire) < max(HRR_pred(i - 1, mslice[:])) * 0.1 and HRR_pred(i, max_fire) < 0.5 * max(HRR_pred(i - 1, mslice[:]))):
