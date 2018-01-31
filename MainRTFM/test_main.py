81e2f302664eccd8f6227ebc57f925b0791a5463
Paul A. Beata <pbeata@users.noreply.github.com>
2018-01-31 10:38:21 -0500

    updated test_main.py to recv new LCM data from QueueSensors

--- MainRTFM/test_main.py
+++ MainRTFM/test_main.py
@@ -1,27 +1,59 @@
-import time
+# Python packages
 import subprocess
+import select
+import time
 
-# I want to launch a C++ program from this Python main program,
-# but I don't want this Python code to wait for C++ program to finish. 
+# LCM packages
+import lcm
+
+# LCM data structures
+from sim_sensor import sensor_data
+
+# global variables (move to input file?)
+global NUM_SENSORS
+
+
+# handle new data coming from sensors
+def sensor_handler(channel, data):
+  timeStamp = time.strftime("%c")
+  msg = sensor_data.decode(data)
+  sid = msg.sensorID
+  print(" --> MAIN RTFM recv %.4f from sensor #%d at %s" % (msg.sendTime, sid, timeStamp))
+
 
 print("\n[START MAIN RTFM]\n")
+# INPUT ===================================================
+NUM_SENSORS = 4
+timeout = 0.01  # amount of time to wait, in seconds
+channelPrefix = "SENSOR"
+execSensor = "exec ../Sensors/QueueSensors.ex"
+# =========================================================
+
+# initialize the LCM library
+lc = lcm.LCM()
+
+# subscribe to all the sensor channels to recv new data
+for i in range(0, NUM_SENSORS):
+  channel = channelPrefix + str(i)
+  lc.subscribe(channel, sensor_handler)
 
 # launch sensor simulator
-execSensor = "exec ./QueueSensors.ex"
 userMsg = 'Ready to launch the sensors? (Enter 0 or 1) '
-
 startSensors = raw_input(userMsg)
 if (startSensors == "1"):
   sensorProcess = subprocess.Popen(execSensor, shell=True)
 else:
   print("***Sensor simulation was not started***")
 
-# do my own work in main while the sensors run:
+# main event handling loop to:
+#   1. recv new data from sensors
+#   2. decide what to do with the data
 checkPoll = None
 while (checkPoll == None):
   checkPoll = sensorProcess.poll()
-  # doing work via sleep
-  time.sleep(0.100)
-  #print("-------------------")
+  rfds, wfds, efds = select.select([lc.fileno()], [], [], timeout)
+  if rfds:
+    lc.handle()
 
+# end of the main script
 print("\n[END MAIN RTFM]\n")
